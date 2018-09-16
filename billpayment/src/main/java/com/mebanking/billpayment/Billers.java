e11d218b6be078dcf1b447e7576b05add8be4c1c
Piyush Mittal <m@piyushmittal.com>
2018-09-16 10:50:34 +0700

    Added Dockerfile

---
+++ billpayment/src/main/java/com/mebanking/billpayment/Billers.java
@@ -0,0 +1,33 @@
+package com.mebanking.billpayment;
+
+public class Billers {
+
+	int id;
+	String billerName;
+	String imageUrl;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBillerName() {
+		return billerName;
+	}
+
+	public void setBillerName(String billerName) {
+		this.billerName = billerName;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+}
