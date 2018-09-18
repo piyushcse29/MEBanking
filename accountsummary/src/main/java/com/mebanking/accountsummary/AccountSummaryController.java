644f6bde9a570f77418c92052ceac8abbb91a9a8
Piyush Mittal <m@piyushmittal.com>
2018-09-18 15:23:22 +0700

    NamingServer initial commit.

--- accountsummary/src/main/java/com/mebanking/accountsummary/AccountSummaryController.java
+++ accountsummary/src/main/java/com/mebanking/accountsummary/AccountSummaryController.java
@@ -3,10 +3,12 @@ package com.mebanking.accountsummary;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RibbonClient(name = "account-summary-service")
 public class AccountSummaryController {
 
 	@Autowired
---
+++ naming-server/src/main/java/com/mebanking/namingserver/NamingServerApplication.java
@@ -0,0 +1,12 @@
+package com.mebanking.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+}
