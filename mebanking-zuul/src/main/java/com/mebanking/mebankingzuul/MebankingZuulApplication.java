449fc8be53b460b6f6a10bc12138701b99eea149
Piyush Mittal <m@piyushmittal.com>
2018-09-21 12:43:57 +0700

    properties update

--- mebanking-zuul/src/main/java/com/mebanking/mebankingzuul/MebankingZuulApplication.java
+++ mebanking-zuul/src/main/java/com/mebanking/mebankingzuul/MebankingZuulApplication.java
@@ -2,8 +2,12 @@ package com.mebanking.mebankingzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class MebankingZuulApplication {
 
 	public static void main(String[] args) {
