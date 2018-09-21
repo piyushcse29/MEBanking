53ebd56cef03713f02c5f7f9678b63fa1a6c5801
Piyush Mittal <m@piyushmittal.com>
2018-09-21 12:34:25 +0700

    Zuul intitial commit

---
+++ mebanking-zuul/src/main/java/com/mebanking/mebankingzuul/MebankingZuulApplication.java
@@ -0,0 +1,12 @@
+package com.mebanking.mebankingzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MebankingZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MebankingZuulApplication.class, args);
+	}
+}
